7a2d2982a9b2c2d755f893ea4b7955c00a1221bb
kokatmx <umyuhm611@gmail.com>
2024-10-16 15:30:53 +0700

    first commit and done

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\FilesController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/files', [FilesController::class,'index'])->name('files');
+Route::get('/download/{filesId}', [FilesController::class, 'downloadFile'])->name('download');
